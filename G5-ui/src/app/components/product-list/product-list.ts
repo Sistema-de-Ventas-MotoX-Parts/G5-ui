dcddc042aad55c39e6fdf9cabcca30ffd70f1735
Ayelen Alva <ayelenalva302@gmail.com>
2026-06-18 12:41:08 -0300

    fix: corrección de errores

--- G5-ui/src/app/components/product-list/product-list.ts
+++ G5-ui/src/app/components/product-list/product-list.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Product } from '../../models/product';
+import { Product } from '../../models/products';
 
 @Component({
   selector: 'app-product-list',
